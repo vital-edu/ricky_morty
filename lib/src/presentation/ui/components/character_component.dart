df62465c97f14ed006401d91a18dabb030ce293b
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-07-16 14:45:41 -0300

    Layout CharacterComponent.

--- lib/src/presentation/ui/components/character_component.dart
+++ lib/src/presentation/ui/components/character_component.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:casino_test/src/data/models/character.dart';
 import 'package:flutter/material.dart';
 
@@ -13,29 +14,41 @@ class CharacterComponent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment.topLeft,
-      padding: EdgeInsets.all(8),
-      child: Container(
-        padding: const EdgeInsets.all(16),
-        width: double.infinity,
-        decoration: ShapeDecoration(
-          color: Color.fromARGB(120, 204, 255, 255),
-          shape: ContinuousRectangleBorder(
-            borderRadius: BorderRadius.circular(32),
+    return DecoratedBox(
+      decoration: ShapeDecoration(
+        color: Color.fromARGB(120, 204, 255, 255),
+        shape: ContinuousRectangleBorder(
+          borderRadius: BorderRadius.circular(32),
+        ),
+      ),
+      child: ListTile(
+        contentPadding: EdgeInsets.all(8),
+        leading: CircleAvatar(
+          backgroundColor: Colors.transparent,
+          backgroundImage: CachedNetworkImageProvider(
+            character.image,
+            maxWidth: 50,
+            maxHeight: 50,
           ),
         ),
-        child: Column(
+        title: Text(character.name),
+        subtitle: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(character.species),
+            Text(
+              character.origin.name,
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+        trailing: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Padding(
               padding: const EdgeInsets.all(2.0),
-              child: Text(character.name),
-            ),
-            Image.network(
-              character.image,
-              width: 50,
-              height: 50,
+              child: Text(character.gender),
             ),
           ],
         ),
