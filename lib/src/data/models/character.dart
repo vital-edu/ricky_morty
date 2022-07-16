7c5dff43ee06f6ae25d575d0d4a92f8f94bac8b5
Eduardo Vital Alencar Cunha <vital@hey.com>
2022-07-16 14:23:36 -0300

    Add all character fields to model.

--- lib/src/data/models/character.dart
+++ lib/src/data/models/character.dart
@@ -4,13 +4,46 @@ part 'character.g.dart';
 
 @JsonSerializable()
 class Character {
+  final int id;
   final String name;
   final String image;
+  final String status;
+  final String type;
+  final String species;
+  final Location origin;
+  final Location location;
+  final List<String> episode;
+  final String url;
+  final String created;
+  final String gender;
 
-  Character(this.name, this.image);
+  Character(
+      this.name,
+      this.image,
+      this.status,
+      this.type,
+      this.species,
+      this.origin,
+      this.location,
+      this.episode,
+      this.url,
+      this.created,
+      this.id,
+      this.gender);
 
   factory Character.fromJson(Map<String, dynamic> json) =>
       _$CharacterFromJson(json);
 
   Map<String, dynamic> toJson() => _$CharacterToJson(this);
+}
+
+@JsonSerializable()
+class Location {
+  final String name;
+  final String url;
+
+  Location({required this.name, required this.url});
+
+  factory Location.fromJson(Map<String, dynamic> json) =>
+      _$LocationFromJson(json);
 }
--- lib/src/data/models/character.g.dart
+++ lib/src/data/models/character.g.dart
@@ -9,9 +9,39 @@ part of 'character.dart';
 Character _$CharacterFromJson(Map<String, dynamic> json) => Character(
       json['name'] as String,
       json['image'] as String,
+      json['status'] as String,
+      json['type'] as String,
+      json['species'] as String,
+      Location.fromJson(json['origin'] as Map<String, dynamic>),
+      Location.fromJson(json['location'] as Map<String, dynamic>),
+      (json['episode'] as List<dynamic>).map((e) => e as String).toList(),
+      json['url'] as String,
+      json['created'] as String,
+      json['id'] as int,
+      json['gender'] as String,
     );
 
 Map<String, dynamic> _$CharacterToJson(Character instance) => <String, dynamic>{
+      'id': instance.id,
       'name': instance.name,
       'image': instance.image,
+      'status': instance.status,
+      'type': instance.type,
+      'species': instance.species,
+      'origin': instance.origin,
+      'location': instance.location,
+      'episode': instance.episode,
+      'url': instance.url,
+      'created': instance.created,
+      'gender': instance.gender,
+    };
+
+Location _$LocationFromJson(Map<String, dynamic> json) => Location(
+      name: json['name'] as String,
+      url: json['url'] as String,
+    );
+
+Map<String, dynamic> _$LocationToJson(Location instance) => <String, dynamic>{
+      'name': instance.name,
+      'url': instance.url,
     };
